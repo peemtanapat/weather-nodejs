0e9dbaee370e890deb2d50e20d6e0766ba9a8f76
peemtanapat <tanapat.c@nellika.co.th>
2020-12-17 22:13:00 +0700

    add template and public files

---
+++ public/js/app.js
@@ -0,0 +1,26 @@
+const weatherForm = document.querySelector('form');
+const search = document.querySelector('input');
+const messageOne = document.querySelector('#message-1');
+const messageTwo = document.querySelector('#message-2');
+
+weatherForm.addEventListener('submit', (event) => {
+  messageOne.textContent = 'Loading';
+  messageTwo.textContent = '';
+
+  event.preventDefault();
+
+  const location = search.value;
+
+  fetch(`/weather?address=${location}`).then((response) => {
+    response.json().then((data) => {
+      if (data.error) {
+        messageOne.textContent = '';
+        messageTwo.textContent = data.error;
+        return console.log({ error: data.error });
+      }
+
+      messageOne.textContent = data.location;
+      messageTwo.textContent = data.forecastData;
+    });
+  });
+});
